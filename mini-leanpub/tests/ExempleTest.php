ef3d63a5fb38c46ed046cc01966228834d3f0213
nanderson castro <nandokstro@gmail.com>
2022-12-26 20:33:36 -0300

    Iniciando projeto e configuracoes iniciais

---
+++ mini-leanpub/tests/ExempleTest.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Test\MiniLeanpub;
+
+use PHPUnit\Framework\TestCase;
+
+class ExempleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function Example()
+    {
+        $this->assertTrue(true);
+    }
+}
