0002e568c7baa08c0e6a4b9c2d8041277e91a029
nanderson castro <nandokstro@gmail.com>
2023-03-14 11:14:52 -0300

    Adicionando pontos para rodar os testes de integracao

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
--- mini-leanpub/app/Infrastructure/Repository/Book/BookEloquentRepository.php
+++ mini-leanpub/app/Infrastructure/Repository/Book/BookEloquentRepository.php
@@ -1,15 +1,25 @@
-<?php 
+<?php
+
 namespace MiniLeanpub\Infrastructure\Repository\Book;
 
 use MiniLeanpub\Domain\Book\Repository\BookRepositoryInterface;
 use App\Models\Book;
+use Illuminate\Support\Str;
 
 class BookEloquentRepository implements BookRepositoryInterface
 {
-    public function __construct(private Book $model){}
+    public function __construct(private Book $model)
+    {
+    }
 
     public function create($data)
     {
+        $data['book_code'] = $data['bookCode'];
+        $data['book_path'] = $data['bookPath'];
+        $data['slug']      = Str::slug($data['title']);
+
+        unset($data['bookCode'], $data['mimeType'], $data['bookPath']);
+
         return $this->model->create($data);
     }
 
