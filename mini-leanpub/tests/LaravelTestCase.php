488ca5ea674711efedbab1d23efd5117d5e4438e
nanderson castro <nandokstro@gmail.com>
2023-03-13 21:17:57 -0300

    Aula 3 Mod 04

---
+++ mini-leanpub/tests/LaravelTestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\MiniLeanpub;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LaravelTestCase extends TestCase
+{
+    use RefreshDatabase;
+}
